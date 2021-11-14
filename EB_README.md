d8997e445b27feebc5f432faa53110bfa6d025ea
Raphael Jambalos <rajambalos@Raphaels-MacBook-Pro.local>
2021-11-14 18:48:11 +0800

    Add eb extensions

---
+++ EB_README.md
@@ -0,0 +1,5 @@
+eb init -p python-3.8 flask-prime --region us-west-2
+
+eb create prime-dev
+
+x
--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import os
 from app import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
